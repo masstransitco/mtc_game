e0dab6f4f638969c255e741a13bd8557fb1c26f3
Neuter <mark@air.city>
2025-01-18 13:01:37 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          'cannon-es': ['cannon-es']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['three', 'cannon-es']
+  },
+  server: {
+    host: true,
+    port: 3000
+  }
+});
